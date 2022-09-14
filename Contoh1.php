e5ffd33f22c50ce2e570af2793a8d4bac8063ae4
inakatrs <113497880+inakatrs@users.noreply.github.com>
2022-09-14 20:32:53 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan";
+        echo"Nama saya inaka trisi sinaga saya tinggal di daerah bekasi olah raga yang saya sukai adalah berenang";
+    }
+}
+
+?>
